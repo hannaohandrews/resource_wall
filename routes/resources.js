33706ba80cf09fa24f941decedfeef2d3301c453
hannaohandrews <hannaohandrews@gmail.com>
2020-07-29 22:33:53 +0000

    added commit and ratings button

--- routes/resources.js
+++ routes/resources.js
@@ -56,9 +56,9 @@ module.exports = (db) => {
     } else {
       const query = {
         text: `
-        SELECT resources.title, resources.resource_url, resources.description, resources.resource_image_url, ROUND(AVG(resources.rating), 1) AS rating, resources.user_id AS user_id
+        SELECT resources.id,resources.title, resources.resource_url, resources.description, resources.resource_image_url, ROUND(AVG(resources.rating), 1) AS rating, resources.user_id AS user_id
         FROM resources WHERE id = $1
-        GROUP BY resources.title, resources.resource_url, resources.description, resources.resource_image_url, resources.rating, resources.user_id`,
+        GROUP BY resources.id,resources.title, resources.resource_url, resources.description, resources.resource_image_url, resources.rating, resources.user_id`,
         values: [id]
       }
       db
@@ -66,7 +66,9 @@ module.exports = (db) => {
       .then(result => {
         const templateVars = {
           resource: result.rows[0],
-          user : req.session.user_id
+          user : req.session.user_id,
+          id : req.params.id
+
         }
         res.render("5_url_desc", templateVars);
       })
@@ -76,6 +78,7 @@ module.exports = (db) => {
 
   // HOA resource url page POST
   router.post("/:id", (req,res) => {
+    console.log('hello')
     if (!req.session.user_id) {
       console.log('user not logged in')
       const templateVars = {
@@ -86,38 +89,45 @@ module.exports = (db) => {
 
     } else {
     const promises = [];
-    console.log('req.body', req.body);
-    console.log('rating',req.body.rating)
-    console.log('user_id',req.session.user_id.rating)
-    console.log('resource id',req.params.id)
+    const resource_id = req.params.id;
+    const user_id = req.session.user_id;
+
+    const category = req.body.category;
+    const rating = req.body.rating;
+    const comment = req.body.comment
+
 
     // const promiseOne = db.query (
     //   'INSERT INTO categories(id) VALUES ($1)',
-    //   [req.body.category])l;
+    //   [req.body.category]);
 
-    //  const promiseTwo = db.query (
-    //  'INSERT INTO ratings(rating, user_id, resource_id) VALUES ($1,$2,$3)',
-    //  [req.body.rating, req.session.user_id,id]);
+     const promiseTwo = db.query (
+     'INSERT INTO ratings(rating, user_id, resource_id) VALUES ($1,$2,$3)',
+     [rating, user_id,resource_id]);
 
 
-     // const promiseThree = db.query (
-    //   'INSERT INTO comments (commeqnt_text, user_id, resource_id) VALUES ($1)',
-    //   [req.body.comment]);
+     const promiseThree = db.query (
+      'INSERT INTO comments (comment_text, user_id, resource_id) VALUES ($1,$2,$3)',
+      [comment,user_id,resource_id]);
 
-    // if (req.body.categories){
-      //promises.push(promiseOne);
-    //}
-    // if (req.body.rating){
-      //promises.push(promiseTwo);
-    //}
+    if (req.body.categories){
+      promises.push(promiseOne);
+    }
+    if (req.body.rating){
+      promises.push(promiseTwo);
+    }
+      if (req.body.comment){
+      promises.push(promiseThree);
+    }
+
     Promise.all(promises)
 
-    // .then(result =>{
-    //console.log('all done'))
-    //   console.log(result.rows[0]);
-    result.redirect("/4_homepage_logged")
-    // })
-    // .catch(err => console.log(err))
+    .then(result =>{
+
+    res.redirect('/login/:id')
+    console.log('redirected')
+    })
+    .catch(err => console.log(err))
     }
   });
 
