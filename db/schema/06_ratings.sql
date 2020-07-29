67b39aaf823e65cc12754def2696616e913bfc6f
hannaohandrews <hannaohandrews@gmail.com>
2020-07-29 20:01:56 +0000

    trying to add comments and rating

--- db/schema/06_ratings.sql
+++ db/schema/06_ratings.sql
@@ -5,3 +5,11 @@ CREATE TABLE ratings (
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE
 );
+
+
+
+  const query= {
+    text:`INSERT INTO resources (title, resource_url, description, resource_image_url, rating, user_id)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING *`, values : [resource.title, resource.url, resource.description, resource.resource_image_url,resource.rating, user]
+    }
--- routes/resources.js
+++ routes/resources.js
@@ -3,6 +3,7 @@ const router  = express.Router();
 
 module.exports = (db) => {
 
+  // CJ Making a new resource  GET
   router.get("/new", (req, res) => {
     if (!req.session.user_id) {
       const templateVars = {
@@ -17,7 +18,34 @@ module.exports = (db) => {
     }
   });
 
-  // CJ resource url page
+ // CJ Making a new resource POST
+  router.post("/new", (req,res) => {
+    if (!req.session.user_id) {
+      const templateVars = {
+        user : req.session.user_id
+      }
+      res.redirect("/",templateVars);
+      return;
+    } else {
+    const resource = req.body
+    const user = req.session.user_id
+    console.log(resource)
+    const query= {
+    text:`INSERT INTO resources (title, resource_url, description, resource_image_url, rating, user_id)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING *`, values : [resource.title, resource.url, resource.description, resource.resource_image_url,resource.rating, user]
+    }
+    db
+    .query(query)
+    .then(result =>
+      res.redirect("/4_homepage_logged")
+    )
+    .catch(err => console.log(err))
+      }
+    });
+
+
+  // CJ resource url page GET
   router.get("/:id", (req,res) => {
     const id = req.params.id;
     if (!req.session.user_id) {
@@ -46,30 +74,54 @@ module.exports = (db) => {
     }
   });
 
-  router.post("/new", (req,res) => {
+  // HOA resource url page POST
+  router.post("/:id", (req,res) => {
     if (!req.session.user_id) {
+      console.log('user not logged in')
       const templateVars = {
         user : req.session.user_id
       }
       res.redirect("/",templateVars);
       return;
+
     } else {
-    const resource = req.body
-    const user = req.session.user_id
-    console.log(resource)
-    const query= {
-    text:`INSERT INTO resources (title, resource_url, description, resource_image_url, rating, user_id)
-    VALUES ($1, $2, $3, $4, $5, $6)
-    RETURNING *`, values : [resource.title, resource.url, resource.description, resource.resource_image_url,resource.rating, user]
+    const promises = [];
+    console.log('req.body', req.body);
+    console.log('rating',req.body.rating)
+    console.log('user_id',req.session.user_id.rating)
+    console.log('resource id',req.params.id)
+
+    // const promiseOne = db.query (
+    //   'INSERT INTO categories(id) VALUES ($1)',
+    //   [req.body.category])l;
+
+    //  const promiseTwo = db.query (
+    //  'INSERT INTO ratings(rating, user_id, resource_id) VALUES ($1,$2,$3)',
+    //  [req.body.rating, req.session.user_id,id]);
+
+
+     // const promiseThree = db.query (
+    //   'INSERT INTO comments (commeqnt_text, user_id, resource_id) VALUES ($1)',
+    //   [req.body.comment]);
+
+    // if (req.body.categories){
+      //promises.push(promiseOne);
+    //}
+    // if (req.body.rating){
+      //promises.push(promiseTwo);
+    //}
+    Promise.all(promises)
+
+    // .then(result =>{
+    //console.log('all done'))
+    //   console.log(result.rows[0]);
+    result.redirect("/4_homepage_logged")
+    // })
+    // .catch(err => console.log(err))
     }
-    db
-    .query(query)
-    .then(result =>
-      res.redirect("/4_homepage_logged")
-    )
-    .catch(err => console.log(err))
-      }
-    });
+  });
+
+
 
   return router;
 };
