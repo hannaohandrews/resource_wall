c287d49d3e6985b871c554aba1c0cce5e2642c79
Caroline <carolinejepps@gmail.com>
2020-07-31 01:41:48 +0000

    Styled regiter page and lik button toggle

--- db/seeds/07_categories.sql
+++ db/seeds/07_categories.sql
@@ -1,11 +1,14 @@
 INSERT INTO categories (title, category_image_url)
-VALUES ('Web Development', 'https://www.codecademy.com/webpack/e908164f059c81faded2866ab983e950.png');
+VALUES ('Coding for kids', 'https://www.codecademy.com/webpack/e908164f059c81faded2866ab983e950.png');
 
 INSERT INTO categories (title, category_image_url)
 VALUES ('Cooking', 'https://www.lazygourmet.ca/wp-content/uploads/classroom-banner.jpg');
 
 INSERT INTO categories (title, category_image_url)
-VALUES ('Early Years', 'https://www.earlyyearscareers.com/eyc/wp-content/uploads/2017/01/education-919895_1280.jpg');
+VALUES ('Maths', 'https://www.earlyyearscareers.com/eyc/wp-content/uploads/2017/01/education-919895_1280.jpg');
 
 INSERT INTO categories (title, category_image_url)
 VALUES ('Languages', 'https://1635225677.rsc.cdn77.org/images/best-language-learning-apps.jpg');
+
+INSERT INTO categories (title, category_image_url)
+VALUES ('Crafts', 'https://www.kidslovewhat.com/wp-content/uploads/2019/02/unicorn-crafts-for-kids.jpg');
